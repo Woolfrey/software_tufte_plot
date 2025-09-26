404e2d97beb26b9604d15a3e5495b608f198e76d
Jon <jonathan.woolfrey@gmail.com>
2025-09-26 17:37:20 +0100

    First commit

---
+++ tufteplotlib/__init__.py
@@ -0,0 +1,9 @@
+from .plots import barcode_plot, rug_plot, scatter_plot, time_series        
+from .styles import apply_tufte_style
+
+# Public API in alphabetical order
+__all__ = ["apply_tufte_style",
+           "barcode_plot"
+           "rug_plot",
+           "scatter_plot",
+           "time_series"]
---
+++ tufteplotlib/plots/__init__.py
@@ -0,0 +1,11 @@
+# tufteplotlib/plots/__init__.py
+from .barcode     import barcode_plot
+from .rug         import rug_plot
+from .scatter     import scatter_plot
+from .time_series import time_series
+
+__all__ = ["barcode_plot"
+           "rug_plot"
+           "scatter_plot"
+           "time_seris"]
+
