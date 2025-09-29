97d4f5f848e96d0f71d13ea9e3240bb391acff29
Jon <jonathan.woolfrey@gmail.com>
2025-09-29 14:06:17 +0100

    Added anscombe

--- tufteplotlib/__init__.py
+++ tufteplotlib/__init__.py
@@ -1,21 +1,23 @@
-from .plots  import add_min_max_colorbar
-from .plots  import barcode_plot
-from .plots  import bar_chart
-from .plots  import density_plot
-from .plots  import galaxy_plot
-from .plots  import histogram_plot
-from .plots  import line_plot
-from .plots  import pareto_chart
-from .plots  import quartile_plot
-from .plots  import rug_plot
-from .plots  import scatter_plot
-from .plots  import sparkline
-from .plots  import stem_and_leaf_plot
-from .plots  import time_series        
-from .styles import apply_tufte_style
+from .datasets import anscombe
+from .plots    import add_min_max_colorbar
+from .plots    import barcode_plot
+from .plots    import bar_chart
+from .plots    import density_plot
+from .plots    import galaxy_plot
+from .plots    import histogram_plot
+from .plots    import line_plot
+from .plots    import pareto_chart
+from .plots    import quartile_plot
+from .plots    import rug_plot
+from .plots    import scatter_plot
+from .plots    import sparkline
+from .plots    import stem_and_leaf_plot
+from .plots    import time_series        
+from .styles   import apply_tufte_style
 
 # Public API in alphabetical order
 __all__ = ["add_min_max_colorbar",
+           "anscombe",
            "apply_tufte_style",
            "barcode_plot",
            "bar_chart",
