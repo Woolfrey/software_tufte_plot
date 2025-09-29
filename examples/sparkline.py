0145be988f6ee8d62af806efed3abf00eca1581e
Jon <jonathan.woolfrey@gmail.com>
2025-09-29 14:04:50 +0100

    Obsolete

--- examples/sparkline.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from tufteplotlib.plots.sparkline import sparkline
-
-y = np.random.normal(0, 1, 30).cumsum()
-
-sparkline(y, show_dots=True, show_labels=True)
-
-plt.show()
